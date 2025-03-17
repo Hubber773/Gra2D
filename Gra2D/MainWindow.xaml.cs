56e6187fee738b4dd7dead6e1bf27064b1da3645
Hubber773 <githubber53@gmail.com>
2025-03-17 22:24:51 +0100

    lista zmian jest w README.md

--- Gra2D/MainWindow.xaml.cs
+++ Gra2D/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,37 +11,38 @@ namespace Gra2D
 {
     public partial class MainWindow : Window
     {
-        // Stałe reprezentujące rodzaje terenu
-        public const int LAS = 1;     // las
-        public const int LAKA = 2;     // łąka
-        public const int SKALA = 3;   // skały
-        public const int ILE_TERENOW = 4;   // ile terenów
-        // Mapa przechowywana jako tablica dwuwymiarowa int
+        public StringBuilder wygenerowanySB = new StringBuilder();
+        //--//Stałe reprezentujące rodzaje terenu
+        public const int LAS = 1;     //--//las
+        public const int LAKA = 2;     //--//łąka
+        public const int SKALA = 3;   //--//skały
+        public const int ILE_TERENOW = 4;   //--//ile terenów
+        //--//Mapa przechowywana jako tablica dwuwymiarowa int
         private int[,] mapa;
         private int szerokoscMapy;
         private int wysokoscMapy;
-        // Dwuwymiarowa tablica kontrolek Image reprezentujących segmenty mapy
+        //--//Dwuwymiarowa tablica kontrolek Image reprezentujących segmenty mapy
         private Image[,] tablicaTerenu;
-        // Rozmiar jednego segmentu mapy w pikselach
+        //--//Rozmiar jednego segmentu mapy w pikselach
         private const int RozmiarSegmentu = 32;
 
-        // Tablica obrazków terenu – indeks odpowiada rodzajowi terenu
-        // Indeks 1: las, 2: łąka, 3: skały
+        //--//Tablica obrazków terenu – indeks odpowiada rodzajowi terenu
+        //--//Indeks 1: las, 2: łąka, 3: skały
         private BitmapImage[] obrazyTerenu = new BitmapImage[ILE_TERENOW];
 
-        // Pozycja gracza na mapie
+        //--//Pozycja gracza na mapie
         private int pozycjaGraczaX = 0;
         private int pozycjaGraczaY = 0;
-        // Obrazek reprezentujący gracza
+        //--//Obrazek reprezentujący gracza
         private Image obrazGracza;
-        // Licznik zgromadzonego drewna
+        //--//Licznik zgromadzonego drewna
         private int iloscDrewna = 0;
         public MainWindow()
         {
             InitializeComponent();
             WczytajObrazyTerenu();
 
-            // Inicjalizacja obrazka gracza
+            //--//Inicjalizacja obrazka gracza
             obrazGracza = new Image
             {
                 Width = RozmiarSegmentu,
@@ -51,32 +53,32 @@ namespace Gra2D
         }
         private void WczytajObrazyTerenu()
         {
-            // Zakładamy, że tablica jest indeksowana od 0, ale używamy indeksów 1-3
+            //--//Zakładamy, że tablica jest indeksowana od 0, ale używamy indeksów 1-3
             obrazyTerenu[LAS] = new BitmapImage(new Uri("las.png", UriKind.Relative));
             obrazyTerenu[LAKA] = new BitmapImage(new Uri("laka.png", UriKind.Relative));
             obrazyTerenu[SKALA] = new BitmapImage(new Uri("skala.png", UriKind.Relative));
         }
 
-        // Wczytuje mapę z pliku tekstowego i dynamicznie tworzy tablicę kontrolek Image
+        //--//Wczytuje mapę z pliku tekstowego i dynamicznie tworzy tablicę kontrolek Image
         private void WczytajMape(string sciezkaPliku)
         {
             try
             {
-                var linie = File.ReadAllLines(sciezkaPliku);//zwraca tablicę stringów, np. linie[0] to pierwsza linia pliku
+                var linie = File.ReadAllLines(sciezkaPliku);//--//zwraca tablicę stringów, np. linie[0] to pierwsza linia pliku
                 wysokoscMapy = linie.Length;
-                szerokoscMapy = linie[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;//zwraca liczbę elementów w tablicy
+                szerokoscMapy = linie[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;//--//zwraca liczbę elementów w tablicy
                 mapa = new int[wysokoscMapy, szerokoscMapy];
 
                 for (int y = 0; y < wysokoscMapy; y++)
                 {
-                    var czesci = linie[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);//zwraca tablicę stringów np. czesci[0] to pierwszy element linii
+                    var czesci = linie[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);//--//zwraca tablicę stringów np. czesci[0] to pierwszy element linii
                     for (int x = 0; x < szerokoscMapy; x++)
                     {
-                        mapa[y, x] = int.Parse(czesci[x]);//wczytanie mapy z pliku
+                        mapa[y, x] = int.Parse(czesci[x]);//--//wczytanie mapy z pliku
                     }
                 }
 
-                // Przygotowanie kontenera SiatkaMapy – czyszczenie elementów i definicji wierszy/kolumn
+                //--//Przygotowanie kontenera SiatkaMapy – czyszczenie elementów i definicji wierszy/kolumn
                 SiatkaMapy.Children.Clear();
                 SiatkaMapy.RowDefinitions.Clear();
                 SiatkaMapy.ColumnDefinitions.Clear();
@@ -90,7 +92,7 @@ namespace Gra2D
                     SiatkaMapy.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(RozmiarSegmentu) });
                 }
 
-                // Tworzenie tablicy kontrolk Image i dodawanie ich do siatki
+                //--//Tworzenie tablicy kontrolk Image i dodawanie ich do siatki
                 tablicaTerenu = new Image[wysokoscMapy, szerokoscMapy];
                 for (int y = 0; y < wysokoscMapy; y++)
                 {
@@ -105,7 +107,7 @@ namespace Gra2D
                         int rodzaj = mapa[y, x];
                         if (rodzaj >= 1 && rodzaj < ILE_TERENOW)
                         {
-                            obraz.Source = obrazyTerenu[rodzaj];//wczytanie obrazka terenu
+                            obraz.Source = obrazyTerenu[rodzaj];//--//wczytanie obrazka terenu
                         }
                         else
                         {
@@ -114,48 +116,48 @@ namespace Gra2D
 
                         Grid.SetRow(obraz, y);
                         Grid.SetColumn(obraz, x);
-                        SiatkaMapy.Children.Add(obraz);//dodanie obrazka do siatki na ekranie
+                        SiatkaMapy.Children.Add(obraz);//--//dodanie obrazka do siatki na ekranie
                         tablicaTerenu[y, x] = obraz;
                     }
                 }
 
-                // Dodanie obrazka gracza – ustawiamy go na wierzchu
+                //--//Dodanie obrazka gracza – ustawiamy go na wierzchu
                 SiatkaMapy.Children.Add(obrazGracza);
-                Panel.SetZIndex(obrazGracza, 1);//ustawienie obrazka gracza na wierzchu
+                Panel.SetZIndex(obrazGracza, 1);//--//ustawienie obrazka gracza na wierzchu
                 pozycjaGraczaX = 0;
                 pozycjaGraczaY = 0;
                 AktualizujPozycjeGracza();
 
                 iloscDrewna = 0;
-                EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
-            }//koniec try
+                L_drewno.Content = "Drewno: " + iloscDrewna;
+            }//--//koniec try
             catch (Exception ex)
             {
                 MessageBox.Show("Błąd wczytywania mapy: " + ex.Message);
             }
         }
 
-        // Aktualizuje pozycję obrazka gracza w siatce
+        //--//Aktualizuje pozycję obrazka gracza w siatce
         private void AktualizujPozycjeGracza()
         {
             Grid.SetRow(obrazGracza, pozycjaGraczaY);
             Grid.SetColumn(obrazGracza, pozycjaGraczaX);
         }
 
-        // Obsługa naciśnięć klawiszy – ruch gracza oraz wycinanie lasu
+        //--//Obsługa naciśnięć klawiszy – ruch gracza oraz wycinanie lasu
         private void OknoGlowne_KeyDown(object sender, KeyEventArgs e)
         {
             int nowyX = pozycjaGraczaX;
             int nowyY = pozycjaGraczaY;
-            //zmiana pozycji gracza
-            if (e.Key == Key.Up) nowyY--;
-            else if (e.Key == Key.Down) nowyY++;
-            else if (e.Key == Key.Left) nowyX--;
-            else if (e.Key == Key.Right) nowyX++;
-            //Gracz nie może wyjść poza mapę
+            //--//zmiana pozycji gracza
+            if (e.Key == Key.W) nowyY--; // Zmieniłem na WASD bo strzałki przechodziły pomiędzy inne przyciski i inne takie obiekty*/
+            else if (e.Key == Key.S) nowyY++;
+            else if (e.Key == Key.A) nowyX--;
+            else if (e.Key == Key.D) nowyX++;
+            //--//Gracz nie może wyjść poza mapę
             if (nowyX >= 0 && nowyX < szerokoscMapy && nowyY >= 0 && nowyY < wysokoscMapy)
             {
-                // Gracz nie może wejść na pole ze skałami
+                //--//Gracz nie może wejść na pole ze skałami
                 if (mapa[nowyY, nowyX] != SKALA)
                 {
                     pozycjaGraczaX = nowyX;
@@ -164,30 +166,78 @@ namespace Gra2D
                 }
             }
 
-            // Obsługa wycinania lasu – naciskamy klawisz C
+            //--//Obsługa wycinania lasu – naciskamy klawisz C
             if (e.Key == Key.C)
             {
-                if (mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)//jeśli gracz stoi na polu lasu
+                if (mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)//--//jeśli gracz stoi na polu lasu
                 {
                     mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
                     tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
                     iloscDrewna++;
-                    EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
+                    L_drewno.Content = "Drewno: " + iloscDrewna;
                 }
             }
         }
 
-        // Obsługa przycisku "Wczytaj mapę"
-        private void WczytajMape_Click(object sender, RoutedEventArgs e)
+        //--//Obsługa przycisku "Wczytaj mapę"
+        private void B_WczytajMape_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog oknoDialogowe = new OpenFileDialog();
             oknoDialogowe.Filter = "Plik mapy (*.txt)|*.txt";
-            oknoDialogowe.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory; // Ustawienie katalogu początkowego
+            oknoDialogowe.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory; //--//Ustawienie katalogu początkowego
             bool? czyOtwartoMape = oknoDialogowe.ShowDialog();
             if (czyOtwartoMape == true)
             {
                 WczytajMape(oknoDialogowe.FileName);
             }
         }
+
+        private void B_wklejTXTGry_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText("mapaWygenerowana.txt", Clipboard.GetText());
+            WczytajMape("mapaWygenerowana.txt");
+        }
+
+        private void B_GenerujLiczbyMapy_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder();
+
+            bool czyWierszeOK = int.TryParse(TB_ileWierszy.Text, out int ileWierszy);
+            bool czyKolumnyOK = int.TryParse(TB_ileKolumn.Text, out int ileKolumn);
+
+            ileWierszy = czyWierszeOK && ileWierszy > 0 ? ileWierszy : 5;
+            ileKolumn = czyKolumnyOK && ileKolumn > 0 ? ileKolumn : 5;
+
+            for (int i = 0; i < ileWierszy; i++)
+            {
+                for (int j = 0; j < ileKolumn; j++)
+                {
+                    sb.Append(rnd.Next(1, 4) + " ");
+                }
+                sb.AppendLine();
+            }
+            MessageBox.Show(sb.ToString());
+            wygenerowanySB = sb;
+        }
+        private void B_Kopiuj_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(wygenerowanySB.ToString());
+            MessageBox.Show("Skopiowano do schowka");
+        }
+
+        private void TB_ileWierszy_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_ileWierszy.Text = "";
+        }
+
+        private void TB_ileKolumn_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_ileKolumn.Text = "";
+        }
     }
 }
+
+
+
+
